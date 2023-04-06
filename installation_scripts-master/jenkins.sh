89fe8ff1963a866232883a7761d5fab6cada7d20
boorlasiva <104689952+boorlasiva@users.noreply.github.com>
2023-04-06 21:53:24 +0530

    jenkins installation updation

--- installation_scripts-master/jenkins.sh
+++ installation_scripts-master/jenkins.sh
@@ -8,13 +8,13 @@ java -version
 
 sudo apt update -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
 
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt update -y
-
-sudo add-apt-repository universe -y
 
 sudo apt-get install jenkins -y
 
@@ -22,8 +22,8 @@ sudo service jenkins start
 
 cat /var/lib/jenkins/secrets/initialAdminPassword
 
-#sudo service jenkins status
+sudo service jenkins status
 
 
-#34.232.26.121
+
 
